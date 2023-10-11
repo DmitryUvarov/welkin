23ebe4245e2ce9766ca885f196a928d0257ea5a1
Dima <uvarov3123@gmail.com>
2023-10-11 20:29:45 +0300

    fix

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -45,6 +45,9 @@ function initSliders() {
 			pagination: {
 				el: '.hero__slider .hero__pagination',
 				clickable: true,
+				renderBullet: function (index, className) {
+					return '<span class="' + className + '">' + (index + 1) + "</span>";
+				  },
 			},
 
 		});
